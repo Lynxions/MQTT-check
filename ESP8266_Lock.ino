88206f9e98cdf80ce69405195151eff5549c74f1
Lynxi <93663504+Lynxions@users.noreply.github.com>
2024-05-15 11:57:30 +0700

    Update ESP8266_Lock.ino

--- ESP8266_Lock.ino
+++ ESP8266_Lock.ino
@@ -2,23 +2,23 @@
 #include <PubSubClient.h>
 
 // Replace the SSID/Password details as per your wifi router
-const char* ssid = "";
+const char* ssid = "International University";
 const char* password = "";
 
 // Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
 // Replace your MQTT Broker IP address here:
-const char* mqtt_server = "";
+const char* mqtt_server = "10.238.55.140";
 
 // Initializes the espClient
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 #define ledPin 2
-#define LOCK_PIN 16 // ESP32 pin GPIO16 (D0): for on and off switch
-#define LOAD_PIN 5 
 
-int ledTestPin = 8;
-int relayPin = 7;
+const byte lock_pin = 5; //ESP32 pin GPIO5 (D1)
+const byte control_pin = 16; //ESP32 pin GPIO16 (D0)
+
+bool lockState = false;
 
 void blink_led(unsigned int times, unsigned int duration){
     for (int i = 0; i < times; i++) {
@@ -39,10 +39,10 @@ void setup_wifi(){
     int c=0;
     while(WiFi.status() != WL_CONNECTED){
         blink_led(2,200); //blink LED twice (for 200ms ON time) to indicate that wifi not connected
-        delay(1000); //
+        delay(100); //
         Serial.print(".");
-        c++;
-        if(c>10){
+        c=c+1;
+        if(c>100){
             ESP.restart(); //restart ESP after 10 seconds
         }        
     }
@@ -96,39 +96,37 @@ void callback(char* topic, byte* message, unsigned int length) {
     String messageTemp;
     
     for (int i = 0; i < length; i++) {
-        Serial.print((char)message[i]);
         messageTemp += (char)message[i];
     }
-Serial.println();
 
-    // Check if a message is received on the topic "rpi/broadcast"
-    
-    
-    // if (String(topic) == "rpi/broadcast") {
-    //     if(messageTemp == "on"){
-    //         Serial.println("Action: blink LED");
-    //         blink_led(1,1250); //blink LED once (for 1250ms ON time)
-    //     }
-    //     else if (messageTemp == "off")
-    //     {   
-    //         Serial.println("Action: LED off");
-    //     }
-        
-    // }
-    
-
-    //Similarly add more if statements to check for other subscribed topics 
+    if (String(topic) == "rpi/broadcast") {
+      if (messageTemp == "on" && !lockState) {
+        digitalWrite(lock_pin, HIGH);
+        lockState = true;
+        Serial.print("Lock is open");
+      }
+      if (messageTemp == "off" && lockState) {
+        digitalWrite(lock_pin, LOW);
+        lockState = false;
+        Serial.print("Lock is closed");
+      }
+    }
+    Serial.println();
 }
-
 
 
 void setup(){
     Serial.begin(115200);
+    
+    pinMode(control_pin, INPUT);
+    pinMode(lock_pin, OUTPUT);
 
     setup_wifi();
     client.setServer(mqtt_server,1883);//1883 is the default port for MQTT server
-    client.setCallback(callback);    
+    client.setCallback(callback);  
+
 }
+
 
 void loop(){
     if (!client.connected()){
@@ -136,28 +134,23 @@ void loop(){
     }
 
     client.loop();
-    /*
-    if (!client.loop()){
-        client.connect("ESP8266_Client1");
-    }
-    */
 
     //function to publish
 
-    int analogReading = analogRead(LOAD_PIN);
-    Serial.print("The Sensor is: ");
+    int analogReading = analogRead(control_pin);
 
     if(analogReading == 0) {
-      Serial.println("off");
+      digitalWrite(lock_pin, LOW);
+      
       char* a = "off";
       char* b = "rpi/broadcast";
       client.publish(b, a, 0);
-    } else {
-      Serial.println("on");
+    }
+    if(analogReading == 1) {
+      digitalWrite(lock_pin, HIGH);
+      
       char* a = "on";
       char* b = "rpi/broadcast";
       client.publish(b, a, 0);
-    }
-    
-    delay(2000);
+    }    
 }
