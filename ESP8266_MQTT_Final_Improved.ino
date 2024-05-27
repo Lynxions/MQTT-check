facf1075b7394cab3c3410fcc7a5de7c6284f13d
Lynxi <93663504+Lynxions@users.noreply.github.com>
2024-05-27 14:24:32 +0700

    Update ESP8266_MQTT_Final_Improved.ino

--- ESP8266_MQTT_Final_Improved.ino
+++ ESP8266_MQTT_Final_Improved.ino
@@ -8,31 +8,28 @@
 const char* ssid = "International University";
 const char* password = "";
 
-//const char* mqtt_server = "broker.hivemq.com";
+const char* mqtt_server = "broker.hivemq.com";
 
-const char* mqtt_server = "10.238.55.140"; //IU
+//const char* mqtt_server = "10.238.55.140"; //IU
 
 //const char* mqtt_server = "10.8.99.120"; //IU STAFF
 
 #define ledPin 2
 
-Ticker publishOffTicker;
+Ticker lockOffTickers[4];
 
 // Initializes the espClient
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-unsigned long lockOnTime[] = {0, 0, 0, 0};
-bool control_lock[] = {false, false, false, false};
-bool lockState[] = {false, false, false, false};
-const byte lock_pin[] = {5, 14, 4, 12}; // D1, D5, D2, D6
+const int lockPins[4] = {5, 14, 4, 12}; // ESP32 pins GPIO5 (D1), GPIO14 (D5), GPIO4 (D2), GPIO12 (D6)
+const char* lockTopics[4] = {"rpi/locker1", "rpi/locker2", "rpi/locker3", "rpi/locker4"};
+
+bool lockStates[4] = {false, false, false, false};
+bool controlLocks[4] = {false, false, false, false};
+unsigned long lockOnTimes[4] = {0, 0, 0, 0};
 
 const long interval = 5000; // interval of 5s
-
-//const byte button = 0; //ESP32 pin GPIO0 (D3)
-// int lock_state = LOW;    // the current state of Lock
-// //int button_state;       // the current state of button
-// int last_button_state;  // the previous state of button
 
 void blink_led(unsigned int times, unsigned int duration){
     for (int i = 0; i < times; i++) {
@@ -52,8 +49,8 @@ void setup_wifi(){
 
     int c=0;
     while(WiFi.status() != WL_CONNECTED){
-        blink_led(2,200); //blink LED twice (for 200ms ON time) to indicate that wifi not connected
-        delay(100); //
+        blink_led(2, 200); //blink LED twice (for 200ms ON time) to indicate that wifi not connected
+        delay(100);
         Serial.print(".");
         c=c+1;
         if(c>50){
@@ -77,7 +74,7 @@ void connect_mqttServer(){
             setup_wifi();
         }
 
-        //now attemt to connect to MQTT server
+        //now attempt to connect to MQTT server
         Serial.print("Attempting MQTT connection...");
 
         // Attempt to connect
@@ -85,11 +82,9 @@ void connect_mqttServer(){
             //attempt successful
             Serial.println("connected");
             // Subscribe to topics here
-            client.subscribe("rpi/locker1");
-            client.subscribe("rpi/locker2");
-            client.subscribe("rpi/locker3");
-            client.subscribe("rpi/locker4");
-            //client.subscribe("rpi/xyz"); //subscribe more topics here
+            for (int i = 0; i < 4; i++) {
+                client.subscribe(lockTopics[i]);
+            }
         } 
         else {
             //attempt not successful
@@ -97,10 +92,29 @@ void connect_mqttServer(){
             Serial.print(client.state());
             Serial.println(" trying again in 2 seconds");
         
-            blink_led(3,200); //blink LED three times (200ms on duration) to show that MQTT server connection attempt failed
+            blink_led(3, 200); //blink LED three times (200ms on duration) to show that MQTT server connection attempt failed
             // Wait 2 seconds before retrying
             delay(2000);
         }    
+    }
+}
+
+void handleLock(int lockIndex, bool turnOn) {
+    if (turnOn) {
+        digitalWrite(lockPins[lockIndex], HIGH);
+        lockStates[lockIndex] = true;
+        lockOnTimes[lockIndex] = millis();
+        Serial.print("Lock ");
+        Serial.print(lockIndex + 1);
+        Serial.println(" is open");
+        controlLocks[lockIndex] = true;
+    } else {
+        digitalWrite(lockPins[lockIndex], LOW);
+        lockStates[lockIndex] = false;
+        Serial.print("Lock ");
+        Serial.print(lockIndex + 1);
+        Serial.println(" is closed");
+        controlLocks[lockIndex] = false;
     }
 }
 
@@ -116,27 +130,14 @@ void callback(char* topic, byte* message, unsigned int length) {
     }
 
     for (int i = 0; i < 4; i++) {
-        String lockerTopic = "rpi/locker" + String(i + 1);
-        if (String(topic) == lockerTopic) {
-            handleLockMessage(i, messageTemp);
-            break;
+        if (String(topic) == lockTopics[i]) {
+            if (messageTemp == "on" && !lockStates[i]) {
+                handleLock(i, true);
+            }
+            if (messageTemp == "off") {
+                handleLock(i, false);
+            }
         }
-    }
-}
-
-void handleLockMessage(int lockIndex, String messageTemp) {
-    if (messageTemp == "on" && !lockState[lockIndex]) {
-        digitalWrite(lock_pin[lockIndex], HIGH);
-        lockState[lockIndex] = true;
-        lockOnTime[lockIndex] = millis(); // store the time when lock is turned on
-        Serial.println("Lock " + String(lockIndex + 1) + " is open");
-        control_lock[lockIndex] = true;
-    }
-    if (messageTemp == "off") {
-        digitalWrite(lock_pin[lockIndex], LOW);
-        lockState[lockIndex] = false;
-        Serial.println("Lock " + String(lockIndex + 1) + " is closed");
-        control_lock[lockIndex] = false;
     }
 }
 
@@ -144,45 +145,27 @@ void setup(){
     Serial.begin(115200);
         
     for (int i = 0; i < 4; i++) {
-        pinMode(lock_pin[i], OUTPUT);
+        pinMode(lockPins[i], OUTPUT);
     }
-
-//    pinMode(button, INPUT_PULLUP);
-
-//    button_state = digitalRead(button);
-
 
     setup_wifi();
-    client.setServer(mqtt_server,1883);//1883 is the default port for MQTT server
+    client.setServer(mqtt_server, 1883); //1883 is the default port for MQTT server
     client.setCallback(callback);  
-
 }
 
-void pubOffTicker(){
-  publishOffTicker.once(5, topicPublishOff); // Set timer for 5 seconds to publish 'off' payload 
+void publishLockOff(int lockIndex) {
+    client.publish(lockTopics[lockIndex], "off", 0);
 }
 
-void topicPublishOff(){
-    char f[] = "off";
+void topicPublishOn() {
     for (int i = 0; i < 4; i++) {
-        if (!control_lock[i]) {
-            String topic = "rpi/locker" + String(i + 1);
-            client.publish(topic.c_str(), f, 0);
+        if (controlLocks[i]) {
+            client.publish(lockTopics[i], "on", 0);
+            controlLocks[i] = false;
+            lockOffTickers[i].once(5, std::bind(publishLockOff, i));
         }
     }
 }
-void topicPublishOn(){
-    char o[] = "on";
-    for (int i = 0; i < 4; i++) {
-        if (control_lock[i]) {
-            String topic = "rpi/locker" + String(i + 1);
-            client.publish(topic.c_str(), o, 0);
-            control_lock[i] = false;  
-            pubOffTicker(); 
-        }
-    }
-}
-
 
 void loop(){
     if (!client.connected()){
@@ -191,27 +174,16 @@ void loop(){
     client.loop();
 
     //function to publish
-    // Function to close locks after interval
     unsigned long currentMillis = millis();
     for (int i = 0; i < 4; i++) {
-        if (lockState[i] && currentMillis - lockOnTime[i] >= interval) {
-            digitalWrite(lock_pin[i], LOW);
-            lockState[i] = false;
-            Serial.println("Lock " + String(i + 1) + " is closed after 5 seconds");
+        if (lockStates[i] && currentMillis - lockOnTimes[i] >= interval) {
+            digitalWrite(lockPins[i], LOW);
+            lockStates[i] = false;
+            Serial.print("Lock ");
+            Serial.print(i + 1);
+            Serial.println(" is closed after 5 seconds");
         }
     }
 
-    // button
-    // last_button_state = button_state;
-    // button_state = digitalRead(button);
-
     topicPublishOn();
-
-    // if(last_button_state == HIGH && button_state == LOW){
-    //   lock_state = !lock_state;
-    //   digitalWrite(lock_pin, lock_state);
-    // }
-    //-------------
-    
-
 }
